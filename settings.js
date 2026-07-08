c3c54c67663a95d1f3c111545431ec07586a67bb
HertsVapes <charlie.manhood@outlook.com>
2026-07-08 22:01:12 +0100

    Delete settings.js

--- settings.js
+++
@@ -1,7 +0,0 @@
-window.HV_SETTINGS = {
-  businessName: "Herts Vapes",
-  whatsappNumber: "447885752823",
-  snapchatUsername: "herts.vps",
-  snapchatUrl: "https://www.snapchat.com/add/herts.vps",
-  defaultOrderMessage: "Hi Herts Vapes, I'd like to place an order."
-};
